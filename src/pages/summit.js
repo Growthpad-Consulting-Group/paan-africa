0f0120428aa5ac28bc6b7357b3cb5888ae29aacc
Ronald <matekwaronald@gmail.com>
2026-07-15 13:44:01 +0300

    Summit meta title update

--- src/pages/summit.js
+++ src/pages/summit.js
@@ -417,7 +417,7 @@ const SummitPage = () => {
   return (
     <>
       <SEO
-        title="PAAN Summit 2026 | Creative & Tech Conference"
+        title="The Africa Borderless Creative Economy Summit 2026 | Nairobi"
         description="Join the inaugural PAAN Summit in Nairobi, Kenya — a groundbreaking event uniting Africa's top creative and tech leaders for three days of innovation, collaboration, and growth. Connect with industry leaders, explore cutting-edge trends, and shape the future of African creativity and technology."
         keywords="PAAN Summit 2026, African creative summit, tech conference Africa, Nairobi summit, Pan-African events, African innovation, creative tech Africa, agency summit Africa, creative leadership, tech leadership, African business summit, innovation conference"
         image="https://ik.imagekit.io/nkmvdjnna/PAAN/summit/summit-hero.webp?updatedAt=1757505455932"
